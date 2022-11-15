9bd3993dde14b05d8667398460bc95d9927834fd
Viachaslau Rouski <vrovsky@gmail.com>
2022-11-15 12:44:38 +0000

    initilization added

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{env, near_bindgen};
+use near_sdk::{env, near_bindgen, AccountId};
 
 const PUZZLE_NUMBER: u8 = 1;
 
@@ -11,19 +11,27 @@ pub struct Contract {
 
 #[near_bindgen]
 impl Contract {
-    pub fn get_puzzle_number(&self) -> u8 {
-        PUZZLE_NUMBER
+    #[init]
+    pub fn new(solution: String) -> Self {
+        Self {
+            crossword_solution: solution,
+        }
     }
 
-    pub fn set_solution(&mut self, solution: String) {
-        self.crossword_solution = solution;
+    pub fn get_solution(&self) -> String {
+        self.crossword_solution.clone()
     }
 
-    pub fn guess_solution(&mut self, solution: String) {
-        if solution == self.crossword_solution {
-            env::log_str("You guessed right!")
+    pub fn guess_solution(&mut self, solution: String) -> bool {
+        let hashed_input = env::sha256(solution.as_bytes());
+        let hashed_input_hex = hex::encode(&hashed_input);
+
+        if hashed_input_hex == self.crossword_solution {
+            env::log_str("You guessed right!");
+            true
         } else {
-            env::log_str("Try again.")
+            env::log_str("Try again.");
+            false
         }
     }
 }
@@ -42,5 +50,31 @@ mod tests {
         let debug_hash_bytes = env::sha256(debug_solution.as_bytes());
         let debug_hash_string = hex::encode(debug_hash_bytes);
         println!("Let's debug: {:?}", debug_hash_string);
+    }
+
+    fn get_context(predecessor: AccountId) -> VMContextBuilder {
+        let mut builder = VMContextBuilder::new();
+        builder.predecessor_account_id(predecessor);
+        builder
+    }
+
+    #[test]
+    fn check_guess_solution() {
+        let alice = AccountId::new_unchecked("alice.testnet".to_string());
+        let context = get_context(alice);
+        testing_env!(context.build());
+        let mut contract = Contract::new(
+            "69c2feb084439956193f4c21936025f14a5a5a78979d67ae34762e18a7206a0f".to_string(),
+        );
+        let mut guess_result = contract.guess_solution("wrong answer here".to_string());
+        assert!(!guess_result, "Expected a failure from the wrong guess");
+        assert_eq!(get_logs(), ["Try again."], "Expected a failure log.");
+        guess_result = contract.guess_solution("near nomicon ref finance".to_string());
+        assert!(guess_result, "Expected the correct answer to return true.");
+        assert_eq!(
+            get_logs(),
+            ["Try again.", "You guessed right!"],
+            "Expected a successful log after the previous failed log."
+        );
     }
 }
